67d0c2845e172a689680f87dc5b341d4744d3800
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2017-08-27 00:34:02 +0200

    Add Footer component

---
+++ src/components/footer/index.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import style from './style';
+
+const Footer = () => (
+  <footer className={style.footer}>
+    <span className={style.heart}>❤</span>, Jesper
+  </footer>
+);
+
+export default Footer;
