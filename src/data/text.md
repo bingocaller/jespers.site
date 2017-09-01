6e60ed768e446a23ecb2014611e399eb81a13383
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2017-09-02 01:31:17 +0200

    Store text as Markdown, render via markdown-it

---
+++ src/data/text.md
@@ -0,0 +1,38 @@
+In case the snazzy URL didn't already clue you in, my name is Jesper. I'm a front-end developer who cares about **code structure**, **maintainability**, **performance**, and **accessibility**. I believe these disciplines go hand in hand and enforce one another.{paragraph}
+
+The more structure you have in your code, the easier it is to maintain it; the easier it is to maintain the code, the more time there you'll have for improving performance, accessibility, and overall polish. {paragraph}
+
+### Work {subject-headline}
+
+Asperiores deserunt, reiciendis commodi necessitatibus nisi voluptatem perspiciatis in atque magni doloremque et eveniet quidem neque illum, modi, dicta cum quasi id a consequatur enim odio amet eos numquam corporis!{paragraph}
+
+### Open source {subject-headline}
+
+Distinctio, tempore repudiandae, excepturi vitae quos eius inventore, harum nesciunt doloribus cumque, temporibus quibusdam nobis impedit possimus nam asperiores accusamus aliquam ad magni iste obcaecati quod quas! Sit dicta, nesciunt.{paragraph}
+
+Necessitatibus omnis sit vero asperiores explicabo dolores saepe animi ad cum recusandae aliquid commodi quasi ullam dignissimos nobis, cumque at veritatis, ipsam quidem libero enim, debitis inventore. In corporis, non.{paragraph}
+
+Asperiores deserunt, reiciendis commodi necessitatibus nisi voluptatem perspiciatis in atque magni doloremque et eveniet quidem neque illum, modi, dicta cum quasi id a consequatur enim odio amet eos numquam corporis!{paragraph}
+
+### Stuff I like working with {subject-headline}
+
+* Sass
+* CSS Grid
+* React/Preact (JS in general)
+* Git
+* Animation
+* SVG
+* Vim
+* Markdown
+{list}
+
+### Ask me about (in no particular order) {subject-headline}
+
+* Coffee
+* Keyboards (the clicky-clacky, typing kind)
+* Games (electronic and otherwise)
+* `zsh`
+* My family
+* Podcasts
+* Automation/&shy;Customisation
+{list}
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,36 +1,13 @@
+const md = require('markdown-it')({
+  html: true,
+  typographer: true,
+}).use(require('markdown-it-classy'));
+
+const content = require('../data/text.md');
+
 const Home = () => (
   <div className="content">
-    <div className="container">
-      <p className="paragraph">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio doloremque, at quasi unde vel dolorum, quam dolorem repellat, impedit quidem atque magnam odit! Officiis, laborum, repudiandae. A odio quasi eum!
-      </p>
-      <p className="paragraph">
-        Provident, assumenda, numquam consequatur itaque harum aliquam ducimus illo. At exercitationem tempore sed eum corrupti quos vel, ex sit nulla minima dolor neque officia reiciendis necessitatibus, veritatis, iusto dolorem perspiciatis.
-      </p>
-      <p className="paragraph">
-        Quia dolores accusantium animi nam nostrum aut laborum, doloribus ducimus nisi quibusdam rerum ad repudiandae excepturi laboriosam delectus cum necessitatibus nihil tempore dignissimos. Labore culpa aspernatur ea alias eos fugiat!
-      </p>
-      <h3 className="subject-headline">Subject 1</h3>
-      <p className="paragraph">
-        Laborum, atque, iste? Error doloribus, nam consectetur, tenetur at officiis fugit voluptas nemo aliquid repellendus eligendi commodi eum tempora possimus eos atque consequatur nesciunt neque? Impedit molestiae repellat, architecto cum.
-      </p>
-      <p className="paragraph">
-        Impedit deleniti ab exercitationem quasi sunt voluptatum porro modi molestiae aut perferendis! Expedita possimus dolor ullam, sint, unde a quos earum ducimus iusto, nobis repellat quod accusantium ea vitae voluptatibus.
-      </p>
-      <p className="paragraph">
-        Saepe natus totam dicta veritatis nobis, quia perspiciatis dolore odio, commodi quo iste vitae voluptatum aliquid ipsa, aut maxime sapiente nostrum architecto. Consectetur illum dolorem libero voluptas iste quod doloribus.
-      </p>
-      <h3 className="subject-headline">Subject 2</h3>
-      <p className="paragraph">
-        Distinctio, tempore repudiandae, excepturi vitae quos eius inventore, harum nesciunt doloribus cumque, temporibus quibusdam nobis impedit possimus nam asperiores accusamus aliquam ad magni iste obcaecati quod quas! Sit dicta, nesciunt.
-      </p>
-      <p className="paragraph">
-        Necessitatibus omnis sit vero asperiores explicabo dolores saepe animi ad cum recusandae aliquid commodi quasi ullam dignissimos nobis, cumque at veritatis, ipsam quidem libero enim, debitis inventore. In corporis, non.
-      </p>
-      <p className="paragraph">
-        Asperiores deserunt, reiciendis commodi necessitatibus nisi voluptatem perspiciatis in atque magni doloremque et eveniet quidem neque illum, modi, dicta cum quasi id a consequatur enim odio amet eos numquam corporis!
-      </p>
-    </div>
+    <div className="container" dangerouslySetInnerHTML={{ __html: md.render(content) }} />
   </div>
 );
 
