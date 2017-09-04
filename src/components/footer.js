3441e46ee650d5f441a986c76a1311e5e475b455
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2017-09-04 22:33:12 +0200

    Add titles to footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,9 +5,9 @@ const Footer = () => (
     <span className="heart">❤</span>, Jesper
     <div className="container">
       <div className="icons-row">
-        <a className="icons-row__icon" href="//github.com/bingocaller"><GithubIcon /></a>
-        <a className="icons-row__icon" href="//twitter.com/bingocallr"><TwitterIcon /></a>
-        <a className="icons-row__icon" href="mailto:jesper.nellemann@gmail.com"><EmailIcon /></a>
+        <a className="icons-row__icon" title="Link to my Github profile" href="//github.com/bingocaller"><GithubIcon /></a>
+        <a className="icons-row__icon" title="Link to my Twitter profile" href="//twitter.com/bingocallr"><TwitterIcon /></a>
+        <a className="icons-row__icon" title="Send me an email" href="mailto:jesper.nellemann@gmail.com"><EmailIcon /></a>
       </div>
     </div>
   </footer>
