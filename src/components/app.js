6118fd0fd6f813736e3ae27e13eccf3df9bd7986
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2017-12-22 23:47:44 +0100

    Add meta description and og:type

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,10 @@ import Home from '../routes/home';
 const App = () => (
   <div id="app" className="app">
     <Helmet
+      meta={[
+        { name: 'description', content: 'The personal website of Jesper Nellemann Jakobsen' },
+        { property: 'og:type', content: 'website' },
+      ]}
       link={[
         { rel: 'stylesheet', href: '//fonts.googleapis.com/css?family=Overpass+Mono:400,700|Spectral:400,700' },
       ]}
